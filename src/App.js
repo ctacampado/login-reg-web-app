e6cecc71bd03a7bbe97bff2e4219c990853f274a
ctacampado <ct.acampado@gmail.com>
2019-10-11 15:23:26 +0800

    antd template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Form, Icon, Input, Button, Checkbox } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const StyledLogo = styled.div`
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header>
+        <StyledLogo></StyledLogo>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div style={{ margin: '16px 0' }}/>
+        <div style={{ background: '#fff', padding: 24, minHeight: 'calc(100vh - 150px)' }}/>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>MyId ©2019 Created by MyTech</Footer>
+    </Layout>
   );
 }
 
